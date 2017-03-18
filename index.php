d045ade3c57a0fb661d640656dc3d45bf97c2578
Min-s <impomc@naver.com>
2017-03-18 15:10:52 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+        <title>연성성당 중고등부</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+	<style>
+		#lol {
+			border:red 10px solid;
+			margin:0px;
+			height:20px;
+			width: 230px;
+			padding:10px;
+}
+	</style>	
+</head>
+<body>
+<header>
+        <h1><a id="title" href="./">연성성당 중고등부</a></h1>
+</header>
+<div id="content">
+        <nav>
+                <ul>
+                        <li><a href="./공지사항/">공지사항</a></li>
+                        <li><a href="./사진방/">사진방^^</a></li>
+                        <li><a href=".  /자유게시판/">자유게시판</a></li>
+                </ul>
+        </nav>
+        <div class="post">
+		<h2>	*!아직 개발중인 웹페이지라 게시물 등록 및 회원가입은 안됩니다!*</h2>
+               	<br>	 안녕하세요. 연성 성당 중고등부 웹페이지 입니다!</br>	
+
+	<br>	이 웹사이트는 중고등부가 어떤 활동을 하는지 부모님들에게 </br>
+	
+	<br>	알리고 중고등부의 추억을 무기한으로 저장하기위해 제작 되었습니다</br>
+		
+	<br>	아직 완벽한 웹페이지가 아니라 잔 버그들이 많겠지만 그럴 경우</br>
+
+	<br>  <h3 id="lol">중고등부 서민수 미카엘에게</h3>  어떤 페이지에서 어떤 버그가 </br>
+		
+	<br>	나타나는지 알려 주시면 바로 고치겠습니다(디버깅) ^^</br>
+
+        </div>
+
+</body>
+</html>
+
